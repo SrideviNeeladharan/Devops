8643911eb56ef4b96460a72f5f9eb0b8e3bf6274
Sridevi Neeladharan <sridevin@Sridevis-MacBook-Pro.local>
2017-02-20 14:28:39 -0800

    first commit

---
+++ client/main.js
@@ -0,0 +1,54 @@
+const myApp = angular.module('myApp', ['ngRoute']);
+
+angular.module('myApp')
+       .config(function($routeProvider){
+    
+    $routeProvider.when('/home', {
+                   templateUrl : 'partials/home.html',
+                   controller  : 'HomeController',
+                   controllerAs: 'HC'
+    })
+                  .when('/about', {
+                   templateUrl : 'partials/about.html',
+                   controller  : 'AboutController',
+                   controllerAs: 'AC'
+    })
+                  .when('/contact',{
+                  templateUrl: 'partials/contact.html',
+                  controller  : 'ContactController',
+                  controllerAs: 'CC'
+    })
+                  .when('/benefits', {
+                  templateUrl: 'partials/benefits.html',
+                  controller  : 'BenefitsController',
+                  controllerAs: 'BC'
+    })
+                  .when('/join',{
+                  templateUrl:'partials/join.html',
+                   controller:'JoinController',
+                   controllerAs:'JC'
+        
+    })
+    
+});
+myApp.controller('HomeController', function($scope){
+    $scope.HomePage ='Welcome!';
+    
+});
+
+myApp.controller('AboutController', function($scope){
+    $scope.aboutPage ='Who are we?';
+    
+});
+myApp.controller('ContactController', function($scope){
+    $scope.contactPage ='Contact us';
+    
+});
+myApp.controller('BenefitsController', function($scope){
+    debugger;
+    $scope.benefitsPage ='WHAT ARE OUR BENEFITS?';
+    
+});
+myApp.controller('JoinController', function($scope){
+    $scope.joinpage = "Come, Work with us";
+});
